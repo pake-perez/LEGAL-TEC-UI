9d1860531c2348bc81cb67d59cac56b921765563
Pake Perez <pakeperez@gmail.com>
2023-04-10 11:03:39 -0600

    change href redirection on the sidemenu

--- src/components/common/SideMenu/SideMenu.jsx
+++ src/components/common/SideMenu/SideMenu.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { SideNav, SideNavItems, SideNavLink, SideNavMenu, SideNavMenuItem } from '@carbon/react';
 import { IbmWatsonKnowledgeCatalog, Help, DocumentSigned, Home } from '@carbon/react/icons';
 
@@ -15,10 +16,16 @@ const SideMenu = () => (
 					Home
 				</SideNavLink>
 				<SideNavMenu renderIcon={NotebookIcon} title="Templates">
-					<SideNavMenuItem href="/templates">Upload Template</SideNavMenuItem>
+					{/* <SideNavMenuItem href="/templates">Upload Template</SideNavMenuItem>\ */}
+					<Link className="link cds--side-nav__link" to="/templates">
+						Upload Template
+					</Link>
 				</SideNavMenu>
 				<SideNavMenu renderIcon={DocumentSignedIcon} title="Documentos">
-					<SideNavMenuItem href="/documentos">Crear Documento</SideNavMenuItem>
+					{/* <SideNavMenuItem href="/documentos">Crear Documento</SideNavMenuItem> */}
+					<Link className="link cds--side-nav__link" to="/documentos">
+						Crear Documento
+					</Link>
 				</SideNavMenu>
 				<SideNavLink renderIcon={HelpIcon} to="javascript:void(0)">
 					Soporte
