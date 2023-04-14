ddd5e82bc194cc980d6cf41a23a9cc6bcce96d77
Pake Perez <pakeperez@gmail.com>
2023-04-14 16:54:37 -0600

    dynamic render and create document from template

--- src/components/DocumentForm/DocumentForm.jsx
+++ src/components/DocumentForm/DocumentForm.jsx
@@ -1,22 +1,18 @@
-import React, { useEffect } from 'react';
-import { useState } from 'react';
+import React from 'react';
+import { useEffect, useState } from 'react';
 import { getFotoMulta } from '../../services/getFotoMulta';
 import { getAvailableTemplates } from '../../services/getAvailableTemplates';
-import { Form, FormGroup, Stack, TextInput, Button, Select, SelectItem } from '@carbon/react';
+import { Form, FormGroup, Stack, TextInput, Button, Select, SelectItem, DatePicker, DatePickerInput } from '@carbon/react';
 
 const DocumentForm = () => {
 	const [availableTemplates, setAvailableTemplates] = useState([]);
 	const [templateOptions, setTemplateOptions] = useState(null);
 	const [selectedTemplate, setSelectedTemplate] = useState(null);
 	const [dynamicForm, setDynamicForm] = useState(null);
+	const [formData, setFormData] = useState({});
+	const [currentModify, setCurrentModify] = useState({});
 
 	const submitInfo = async () => {
-		let formData = {
-			name,
-			plate,
-			model,
-		};
-
 		let data = await getFotoMulta(formData);
 		const url = window.URL.createObjectURL(new Blob([data]));
 		const link = document.createElement('a');
@@ -26,27 +22,9 @@ const DocumentForm = () => {
 		link.click();
 	};
 
-	const handleTextChange = (event) => {
-		let newText = event.target.value;
-		switch (event.target.id) {
-			case 'name':
-				setName(newText);
-				break;
-			case 'plate':
-				setPlate(newText);
-				break;
-			case 'model':
-				setModel(newText);
-				break;
-			default:
-				break;
-		}
-	};
-
 	const getTemplatesList = async () => {
 		try {
 			let data = await getAvailableTemplates('pakeperez@gmail.com');
-			console.log(data);
 			setAvailableTemplates(data);
 		} catch (error) {
 			//TODO: Send notification or something else
@@ -67,40 +45,77 @@ const DocumentForm = () => {
 		}
 	}, [availableTemplates]);
 
-	const handleSelectTemplate = (event) => {
-		let templateId = event.target.value;
-		let selected = availableTemplates.find((template) => {
-			return template.id === templateId;
-		});
-		console.log(selected);
-		setSelectedTemplate(selected);
-	};
-
-	const buildForm = () => {
-		let templateFields = selectedTemplate.fields;
-		let fieldKeys = Object.keys(templateFields);
-		let formItems = fieldKeys.map((fieldKey) => {
-			let field = templateFields[fieldKey];
-			let dataType = field.dataType;
-			if (dataType === 'string') {
-				return (
-					<>
-						<div className="cds--content modal-summary-header-text">{field.description}</div>
-						<TextInput onChange={handleTextChange} id={`text-${field.placeholder}`} hideLabel={true} />
-					</>
-				);
-			}
-			// else if(dataType==='number'){}
-			// else if (dataType === 'date') { }
-		});
-		setDynamicForm(<FormGroup style={{ maxWidth: '400px' }}>{formItems}</FormGroup>);
-	};
-
 	useEffect(() => {
 		if (selectedTemplate) {
 			buildForm();
 		}
 	}, [selectedTemplate]);
+
+	useEffect(() => {
+		if (currentModify) {
+			let currentData = { ...formData.values };
+			currentData[currentModify.field] = currentModify.value;
+			setFormData({ ...formData, values: currentData });
+		}
+	}, [currentModify]);
+
+	const handleSelectTemplate = (event) => {
+		let templateId = event.target.value;
+		let selected = availableTemplates.find((template) => {
+			return template.id === templateId;
+		});
+		setSelectedTemplate(selected);
+	};
+
+	const handleTextChange = (event) => {
+		let target = event.target;
+		let newText = target.value;
+		let fieldKey = target.id.split('-')[1];
+		let currentData = { ...formData.values };
+
+		setCurrentModify({ field: fieldKey, value: newText });
+	};
+
+	const handleDateChange = (fieldKey, date) => {
+		let dateObj = date[0];
+		let currentData = { ...formData.values };
+		currentData[fieldKey] = date;
+
+		setCurrentModify({ field: fieldKey, value: dateObj.toDateString() });
+	};
+
+	const buildForm = () => {
+		let templateFields = selectedTemplate.fields;
+		let fieldKeys = Object.keys(templateFields);
+		let newObj = {};
+		newObj.values = {};
+		let formItems = fieldKeys.map((fieldKey) => {
+			let field = templateFields[fieldKey];
+			let dataType = field.dataType;
+			newObj.values[fieldKey] = true;
+			if (dataType === 'string') {
+				return (
+					<>
+						<div className="cds--content modal-summary-header-text">{field.description}</div>
+						<TextInput id={`text-${fieldKey}`} labelText="" hideLabel={true} onChange={handleTextChange} />
+					</>
+				);
+			}
+			// else if(dataType==='number'){}
+			else if (dataType === 'date') {
+				return (
+					<>
+						<div className="cds--content modal-summary-header-text">{field.description}</div>
+						<DatePicker datePickerType="single" onChange={handleDateChange.bind(null, fieldKey)}>
+							<DatePickerInput id={`date`} placeholder="mm/dd/yyyy" hideLabel={true} labelText="" />
+						</DatePicker>
+					</>
+				);
+			}
+		});
+		setFormData(newObj);
+		setDynamicForm(<FormGroup style={{ maxWidth: '400px' }}>{formItems}</FormGroup>);
+	};
 
 	return (
 		<>
